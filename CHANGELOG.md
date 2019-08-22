25ee554f230cea25d3ff64f5b0eca86b982d0eb4
Emiliano Mettini <mettini@gmail.com>
2019-08-22 17:27:50 -0300

    call() and authenticatedCall() now with optional http method parameter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
 [1.0.2] Default http.Client() in case no client by parameter
 [1.0.3] More on: Default http.Client() in case no client by parameter
 [1.0.4] Bugfix grant type in token api call
+[1.0.5] call() and authenticatedCall() now with optional http method parameter
--- lib/gigantier.dart
+++ lib/gigantier.dart
@@ -7,13 +7,15 @@ import 'package:gigantier_sdk/exceptions.dart';
 import 'package:gigantier_sdk/preferences.dart';
 import 'package:http/http.dart' as http;
 
+enum HttpMethod { post, get, put, delete }
+
 class Gigantier {
   static final String grantTypeApp = 'client_credentials';
   static final String grantTypeUser = 'password';
   static final String grantTypeRefresh = 'refresh_token';
   static final String oauthPath = '/OAuth/token';
   static final int retries = 3;
-  
+
   final String hostname;
   final String clientId;
   final String clientSecret;
@@ -26,17 +28,20 @@ class Gigantier {
   http.Client _apiClient;
 
   final prefs = Preferences();
-  
-  Gigantier(
-    this.hostname, 
-    this.clientId, 
-    this.clientSecret, 
-    this.scope,
-    this.appName,
-    { this.protocol = 'https', this.apiVersion = 'v1', this.client }
-  );
 
-  get _baseUrl => '$protocol://$hostname/api${apiVersion != '' ? '/$apiVersion' : ''}';
+  Gigantier(
+    this.hostname,
+    this.clientId,
+    this.clientSecret,
+    this.scope,
+    this.appName, {
+    this.protocol = 'https',
+    this.apiVersion = 'v1',
+    this.client,
+  });
+
+  get _baseUrl =>
+      '$protocol://$hostname/api${apiVersion != '' ? '/$apiVersion' : ''}';
 
   http.Client _getClient() {
     if (_apiClient == null) {
@@ -45,58 +50,98 @@ class Gigantier {
     return _apiClient;
   }
 
-  static Future<Map<String, String>> baseHeaders(appName) async {    
+  static Future<Map<String, String>> baseHeaders(appName) async {
     final headers = Map<String, String>();
     headers['X-GIGANTIER-SDK-LANGUAGE'] = 'Flutter';
-    headers['X-GIGANTIER-SDK-VERSION'] = '1.0.4'; // TODO: obtain version from pubspec.yaml
+    headers['X-GIGANTIER-SDK-VERSION'] =
+        '1.0.5'; // TODO: obtain version from pubspec.yaml
     headers['X-GIGANTIER-APPLICATION'] = appName;
     return Future.value(headers);
   }
 
   Future<Credential> authenticate(String identifier, String password) async {
-    final body = { 'username': identifier, 'password': password };
+    final body = {'username': identifier, 'password': password};
     final credential = await _retrieveToken(grantTypeUser, body);
     await _onCredential(credential);
     return credential;
   }
 
-  Future<Map<String, dynamic>> call(String uri, { Map<String, dynamic> body }) async {
+  Future<Map<String, dynamic>> call(
+    String uri, {
+    Map<String, dynamic> body,
+    HttpMethod method = HttpMethod.post,
+  }) async {
     final appToken = await getAppToken();
     final Map<String, dynamic> requestBody = body != null ? body : {};
     requestBody['access_token'] = appToken;
-    return _execPost(uri, false, retries, body: requestBody);
+    return _execMethod(uri, false, retries, method, body: requestBody);
   }
 
-  Future<Map<String, dynamic>> authenticatedCall(String uri, { Map<String, dynamic> body }) async {
+  Future<Map<String, dynamic>> authenticatedCall(
+    String uri, {
+    Map<String, dynamic> body,
+    HttpMethod method = HttpMethod.post,
+  }) async {
     final userToken = await _getUserToken();
     final Map<String, dynamic> requestBody = body != null ? body : {};
     requestBody['access_token'] = userToken;
-    return _execPost(uri, true, retries, body: requestBody);
+    return _execMethod(uri, true, retries, method, body: requestBody);
   }
 
-  Future<Map<String, dynamic>> _execPost(String uri, bool isUserApi, int retries, { Map<String, dynamic> body }) async {
+  Future<Map<String, dynamic>> _execMethod(
+    String uri,
+    bool isUserApi,
+    int retries,
+    HttpMethod method, {
+    Map<String, dynamic> body,
+  }) async {
     final headers = await baseHeaders(appName);
-    return _getClient().post('$_baseUrl$uri', headers: headers, body: body).then((response) async {
-      final code = response.statusCode;
-      final responseBody = Map<String, dynamic>.from(json.decode(response.body));
+    final url = '$_baseUrl$uri';
+    Future<http.Response> call;
 
-      if (code == 401 && retries > 0 && isUserApi) {
-        final String userToken = await _getUserToken(renew: true);
-        body['access_token'] = userToken;
-        return _execPost(uri, isUserApi, retries + 1, body: body);
-      }
-      else if (code == 401 && retries > 0) {
-        final String appToken = await getAppToken(renew: true);
-        body['access_token'] = appToken;
-        return _execPost(uri, isUserApi, retries - 1, body: body);
-      }
-      else if (code >= 400) throw _buildApiError(responseBody);
+    final _client = _getClient();
 
-      return responseBody;
+    if (method == HttpMethod.post)
+      call = _client.post(url, headers: headers, body: body);
+    else if (method == HttpMethod.delete)
+      call = _client.delete(url, headers: headers);
+    else if (method == HttpMethod.get)
+      call = _client.get(url, headers: headers);
+    else if (method == HttpMethod.put)
+      call = _client.put(url, headers: headers, body: body);
+    else
+      throw Exception('missing http method parameter');
+
+    return call.then((response) {
+      return _onResponse(uri, isUserApi, retries, method, response, body: body);
     });
   }
 
-  Future<String> getAppToken({ bool renew = false }) async {
+  Future<Map<String, dynamic>> _onResponse(
+    String uri,
+    bool isUserApi,
+    int retries,
+    HttpMethod method,
+    http.Response response, {
+    Map<String, dynamic> body,
+  }) async {
+    final code = response.statusCode;
+    final responseBody = Map<String, dynamic>.from(json.decode(response.body));
+
+    if (code == 401 && retries > 0 && isUserApi) {
+      final String userToken = await _getUserToken(renew: true);
+      body['access_token'] = userToken;
+      return _execMethod(uri, isUserApi, retries + 1, method, body: body);
+    } else if (code == 401 && retries > 0) {
+      final String appToken = await getAppToken(renew: true);
+      body['access_token'] = appToken;
+      return _execMethod(uri, isUserApi, retries - 1, method, body: body);
+    } else if (code >= 400) throw _buildApiError(responseBody);
+
+    return responseBody;
+  }
+
+  Future<String> getAppToken({bool renew = false}) async {
     final storedAppToken = await prefs.getAppToken();
     final isAppTokenExpired = await prefs.isAppTokenExpired();
     final notEmptyAppToken = storedAppToken != null && storedAppToken != "";
@@ -113,8 +158,8 @@ class Gigantier {
       });
     }
   }
- 
-  Future<String> _getUserToken({ bool renew = false }) async {
+
+  Future<String> _getUserToken({bool renew = false}) async {
     final storedUserToken = await prefs.getUserToken();
     final isUserTokenExpired = await prefs.isUserTokenExpired();
     final notEmptyUserToken = storedUserToken != null && storedUserToken != "";
@@ -123,11 +168,13 @@ class Gigantier {
       return storedUserToken;
     } else {
       final refreshToken = await prefs.getUserRefreshToken();
-      if (refreshToken == null) throw _buildApiError({ 
-        'error': 'missing_credentials', 
-        'error_description': 'Missing autenticate() call before authenticatedCall()' 
-      });
-      final body = { 'refresh_token': refreshToken };
+      if (refreshToken == null)
+        throw _buildApiError({
+          'error': 'missing_credentials',
+          'error_description':
+              'Missing autenticate() call before authenticatedCall()',
+        });
+      final body = {'refresh_token': refreshToken};
       final credential = await _retrieveToken(grantTypeRefresh, body);
       await _onCredential(credential);
       return credential.accessToken;
@@ -141,9 +188,10 @@ class Gigantier {
     await prefs.setUserTokenExpiration(credential.expires);
   }
 
-  Future<Credential> _retrieveToken(String grantType, Map<String, dynamic> extraBody) async {
+  Future<Credential> _retrieveToken(
+      String grantType, Map<String, dynamic> extraBody) async {
     final Map<String, dynamic> requestBody = {
-      'grant_type': grantType, 
+      'grant_type': grantType,
       'client_id': clientId,
       'client_secret': clientSecret,
       'scope': scope,
@@ -151,7 +199,11 @@ class Gigantier {
     requestBody.addAll(extraBody);
 
     final headers = await baseHeaders(appName);
-    final response = await _getClient().post('$_baseUrl$oauthPath', headers: headers, body: requestBody);
+    final response = await _getClient().post(
+      '$_baseUrl$oauthPath',
+      headers: headers,
+      body: requestBody,
+    );
 
     final body = Map<String, dynamic>.from(json.decode(response.body));
     final ok = body['ok'] as bool;
@@ -159,14 +211,16 @@ class Gigantier {
     if (!ok) throw _buildApiError(body);
 
     return Credential(
-      accessToken: body['access_token'] as String, 
+      accessToken: body['access_token'] as String,
       refreshToken: body['refresh_token'] as String,
-      expires: body['expires_in'] as int
+      expires: body['expires_in'] as int,
     );
   }
 
   _buildApiError(Map<String, dynamic> body) {
-    return ApiErrorException(body['error'] as String, body['error_description'] as String);
+    return ApiErrorException(
+      body['error'] as String,
+      body['error_description'] as String,
+    );
   }
-
 }
